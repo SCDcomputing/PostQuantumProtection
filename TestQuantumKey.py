92a636bbfae0145c49b4dc6b71162a8109a816e4
SCDcomputing <jerzboydrip@gmail.com>
2026-07-18 01:20:57 -0400

    Create TestQuantumKey.py

---
+++ TestQuantumKey.py
@@ -0,0 +1,16 @@
+import unittest
+from quantum_key_gen import generate_quantum_resistant_key
+
+class TestQuantumKeyGen(unittest.TestCase):
+
+    def test_key_generation(self):
+        key = generate_quantum_resistant_key(64)
+        self.assertEqual(len(key), 128)
+
+    def test_key_uniqueness(self):
+        key1 = generate_quantum_resistant_key(64)
+        key2 = generate_quantum_resistant_key(64)
+        self.assertNotEqual(key1, key2)
+
+if __name__ == '__main__':
+    unittest.main()
